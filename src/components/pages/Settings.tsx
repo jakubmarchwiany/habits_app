758a9e4b20abf926e71bd79982c877f674ee6fde
xkvbikx <jakubmarchwiany@icloud.com>
2023-06-09 22:07:16 +0200

    delete user selector

--- src/components/pages/Settings.tsx
+++ src/components/pages/Settings.tsx
@@ -1,10 +1,6 @@
-import { Stack, Switch, Typography } from "@mui/material";
-import useStateUser, { User } from "hooks/use_state_user";
+import { Stack, Typography } from "@mui/material";
 
 function Settings() {
-    const [user, setUser] = useStateUser();
-
-    console.log(user);
     return (
         <Stack
             component="main"
@@ -16,15 +12,6 @@ function Settings() {
             <Typography typography="h2" mt={5}>
                 Ustawienia
             </Typography>
-
-            <Typography typography="h5" mt={3}>
-                Kto korzysta z aplikacji?
-            </Typography>
-            <Stack direction="row" spacing={2} alignItems="center" mt={1}>
-                <Typography>Kuba</Typography>
-                <Switch checked={user === User.Julia && true} onChange={setUser} />
-                <Typography>Julka</Typography>
-            </Stack>
         </Stack>
     );
 }
