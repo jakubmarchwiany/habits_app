6bfe3a858256877fb86bb4499eb36ab4b9ff521c
xkvbikx <jakubmarchwiany@icloud.com>
2023-06-09 22:16:08 +0200

    improve style

--- src/components/pages/Error.tsx
+++ src/components/pages/Error.tsx
@@ -1,24 +1,28 @@
-import { Container, Link, Typography } from "@mui/material";
+import { Container, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
 
 function Error() {
     return (
         <Container
             sx={{
                 display: "flex",
+                backgroundColor: "#424242",
                 flexDirection: "column",
                 alignItems: "center",
                 justifyContent: "center",
                 minHeight: "100vh",
+                minWidth: "100vw",
+                color: "gray",
             }}
         >
-            <Typography sx={{ fontSize: "2rem", marginBottom: "1rem" }} variant="h2" component="h1">
-                Page Not Found
+            <Typography sx={{ fontSize: "5vw", marginBottom: "1rem" }}>
+                Nie znaleziono strony
             </Typography>
-            <Typography sx={{ fontSize: "1rem", marginBottom: "1rem" }} variant="body1">
-                Oops! The page you are looking for could not be found.
+            <Typography sx={{ fontSize: "3vw", marginBottom: "1rem" }}>
+                Ups! Strona, której szukasz, nie została znaleziona.
             </Typography>
-            <Typography sx={{ fontSize: "1rem", marginBottom: "1rem" }} variant="body1">
-                Go back to <Link href="/">homepage</Link>.
+            <Typography sx={{ fontSize: "4vw", marginBottom: "1rem" }}>
+                <Link to="/">Wróć do strony głównej</Link>
             </Typography>
         </Container>
     );
