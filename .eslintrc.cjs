03dd3c6c6442313b6fd32d73f4a8d7bf53399f22
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-09-26 20:39:53 +0200

    update config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,58 @@
+/*global __dirname,module*/
+
 module.exports = {
-  env: { browser: true, es2020: true, node: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': 'warn',
-  },
-}
+    extends: [
+        "eslint:recommended",
+        "plugin:typescript-sort-keys/recommended",
+        "plugin:react/jsx-runtime",
+        "plugin:react-hooks/recommended"
+    ],
+    overrides: [
+        {
+            extends: [
+                "plugin:@typescript-eslint/recommended",
+                "plugin:@typescript-eslint/recommended-requiring-type-checking"
+            ],
+            files: ["**/*.{ts,tsx}"],
+            parserOptions: {
+                tsconfigRootDir: __dirname,
+                project: ["./tsconfig.json"]
+            },
+            rules: {
+                // I only disabled these so that we wouldn't see later rules
+                // show up in earlier files... Don't copy these disables! 😉
+                "@typescript-eslint/await-thenable": "off",
+                "@typescript-eslint/no-floating-promises": "off",
+                "@typescript-eslint/no-misused-promises": "off",
+                "@typescript-eslint/no-unused-vars": ["off", {}],
+                "react-hooks/exhaustive-deps": "off"
+            }
+        },
+        {
+            files: "*.json",
+            parser: "jsonc-eslint-parser",
+            rules: {
+                "jsonc/sort-keys": "error"
+            },
+            extends: ["plugin:jsonc/recommended-with-json"]
+        }
+    ],
+    parser: "@typescript-eslint/parser",
+    plugins: [
+        "@typescript-eslint",
+        "react",
+        "react-hooks",
+        "simple-import-sort",
+        "typescript-sort-keys"
+    ],
+    root: true,
+    rules: {
+        "simple-import-sort/exports": "error",
+        "simple-import-sort/imports": "error"
+    },
+    settings: {
+        react: {
+            version: "detect"
+        }
+    }
+};
--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,24 @@
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
     server: {
-        port: 3000,
         host: true,
+        port: 3000
     },
     build: {
         outDir: "./build",
+        rollupOptions: {
+            output: {
+                manualChunks(id) {
+                    if (id.includes("node_modules")) {
+                        return id.toString().split("node_modules/")[1].split("/")[0].toString();
+                    }
+                }
+            }
+        }
     },
-
-    plugins: [react(), tsconfigPaths()],
+    plugins: [react(), tsconfigPaths()]
 });
