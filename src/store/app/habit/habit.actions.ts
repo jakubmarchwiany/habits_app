c3355b9960b914cbf401a96fde642db3652402a8
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-12-13 12:06:34 +0100

    fix: wrong url navigation

--- src/store/app/habit/habit.actions.ts
+++ src/store/app/habit/habit.actions.ts
@@ -107,7 +107,7 @@ export const createHabitAction =
 
 			appDispatch(appActions.createHabit(habitExt));
 
-			navigate("/settings/groupsOfHabitsManager");
+			navigate("/settings/groups-of-habits-manager");
 		});
 	};
 
