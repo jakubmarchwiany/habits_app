b2c4e788a82001bee6548e59700bfe97290438aa
jakubmarchwiany <jakubmarchwiany@icloud.com>
2023-12-18 17:45:18 +0100

    feat: add new design
    + change radius levels
    + add score background
    + change icon style

--- src/pages/habitsv2/components/Habitv2.tsx
+++ src/pages/habitsv2/components/Habitv2.tsx
@@ -1,4 +1,4 @@
-import { Stack, Typography } from "@mui/material";
+import { Stack, Tooltip, Typography, Zoom } from "@mui/material";
 import { useAppSelector } from "hooks/redux";
 
 import { Activitiesv2 } from "./Activitiesv2";
@@ -18,16 +18,53 @@ export function Habitv2({ _id }: Props): JSX.Element {
 		return <></>;
 	}
 
-	const { activities, description, name, periodInDays, score: goalRate } = habit;
+	const { activities, description, emoji, name, periodInDays, score: goalRate } = habit;
 
 	return (
-		<Stack direction="row" p={1} py={2}>
+		<Stack
+			borderRadius={7}
+			boxShadow={5}
+			className="test"
+			direction="row"
+			my={1}
+			px={2}
+			py={1.5}
+			sx={{
+				background: `linear-gradient(8deg, rgba(0,173, 95,${goalRate / 1.5}) ${
+					goalRate * 100 - 5
+				}%, rgba(211, 49, 49,${goalRate === 0 ? 0 : (1 - goalRate) / 1.5}) ${
+					goalRate * 100
+				}%)`
+			}}
+		>
 			<Stack alignItems="center" direction="row" id="card" mr={1}>
-				<Typography sx={{ fontSize: "75px", lineHeight: 1 }}>🌇</Typography>
+				<Tooltip
+					TransitionComponent={Zoom}
+					placement="top"
+					sx={{ fontSize: "50px" }}
+					title={
+						<>
+							<Typography variant="body1">{description}</Typography>
+							<Typography variant="body1">Co {periodInDays} dni</Typography>
+						</>
+					}
+				>
+					<Typography
+						sx={{
+							borderRadius: 5,
+							boxShadow: 5,
+							fontSize: "50px",
+							lineHeight: 1,
+							p: 1.5
+						}}
+					>
+						{emoji}
+					</Typography>
+				</Tooltip>
 
 				<div id="info">
 					<Typography mx={2} noWrap variant="h4">
-						{name.substring(0, name.length - 2)}
+						{name}
 					</Typography>
 				</div>
 			</Stack>
