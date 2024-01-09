64ff8e8bd18303fd5037d579578490f316d43832
jakubmarchwiany <jakubmarchwiany@icloud.com>
2024-01-09 09:28:08 +0100

    refactor: change deprecate AnyAction to UnknownAction

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,5 +1,5 @@
 import { configureStore } from "@reduxjs/toolkit";
-import { AnyAction } from "redux";
+import { UnknownAction } from "redux";
 import { ThunkAction } from "redux-thunk";
 
 import { appSliceReducers } from "./app/app.slice";
@@ -14,7 +14,7 @@ const store = configureStore({
 
 type RootState = ReturnType<typeof store.getState>;
 type AppDispatch = typeof store.dispatch;
-type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, undefined, AnyAction>;
+type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, undefined, UnknownAction>;
 
 export { store };
 export type { AppDispatch, AppThunk, RootState };
